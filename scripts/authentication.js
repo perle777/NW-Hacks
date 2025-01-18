0e0d321ee296be96a4f8c353062dd0bfe463a8f8
Stella Hui <stellasylviahui@hotmail.com>
2025-01-18 15:02:01 -0800

    hotfix

--- scripts/authentication.js
+++ scripts/authentication.js
@@ -1,33 +1,33 @@
 var ui = new firebaseui.auth.AuthUI(firebase.auth());
 var uiConfig = {
   callbacks: {
-    signInSuccessWithAuthResult: function(authResult, redirectUrl) {
+    signInSuccessWithAuthResult: function (authResult, redirectUrl) {
       var user = authResult.user;
       if (authResult.additionalUserInfo.isNewUser) {
-        db.collection("users").doc(user.uid).set({
-          name: user.displayName,
-          email: user.email,
-        }).then(function() {
-          console.log("New user added to Firestore");
-          window.location.assign("index.html");
-        }).catch(function(error) {
-          console.log("Error adding new user: ", error);
-        });
-        return false; // Prevent redirect if it's a new user
-      } else {
-        return true; // Redirect for existing users
+        db.collection("users")
+          .doc(user.uid)
+          .set({
+            name: user.displayName,
+            email: user.email,
+          })
+          .then(function () {
+            console.log("New user added to Firestore");
+            window.location.assign("index.html");
+          })
+          .catch(function (error) {
+            console.log("Error adding new user: ", error);
+          });
       }
+      window.location.assign("../home/home.html");
+      return false;
     },
-    uiShown: function() {
-      document.getElementById('loader').style.display = 'none';
-    }
+    uiShown: function () {
+      document.getElementById("loader").style.display = "none";
+    },
   },
-  signInFlow: 'popup',
-  signInSuccessUrl: "../dashboard/dashboard.html", // After successful login, redirect here
-  signInOptions: [
-    firebase.auth.EmailAuthProvider.PROVIDER_ID
-  ]
+  signInFlow: "popup",
+  signInSuccessUrl: "../home/home.html",
+  signInOptions: [firebase.auth.EmailAuthProvider.PROVIDER_ID],
 };
 
-// Start Firebase UI
-ui.start('#firebaseui-auth-container', uiConfig);
+ui.start("#firebaseui-auth-container", uiConfig);
