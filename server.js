b2bd1149d58bf9db114d3696c12164e619276f32
Stella Hui <stellasylviahui@hotmail.com>
2025-01-19 09:26:17 -0800

    update file directory

--- server.js
+++ server.js
@@ -11,6 +11,10 @@ app.use(express.json());
 // Serve static files from the "public" directory
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.get('/', (req, res) => {
+    res.redirect('/home'); // Redirect to the home page
+});
+
 // Define routes for serving HTML pages
 app.get('/journal', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'journal.html'));
